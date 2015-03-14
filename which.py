f5b56dc605e1a89ab04f77452be471fe4b2f880d
galaxyjim <galaxyjim@yahoo.com>
2015-03-14 10:31:29 -0400

    initial version

---
+++ which.py
@@ -0,0 +1,23 @@
+
+import os
+import sys
+import glob
+
+#
+targets = sys.argv[1:]
+
+# get directories in path
+pathvar = os.getenv('PATH')
+dirs = pathvar.split(';')
+dirs.append(".")
+
+# search directories
+for onedir in dirs:
+     # for each requested item
+     for name in targets:
+          fullpath = onedir + "\\" + name
+
+          names = glob.glob(fullpath)
+          for name in names:
+               print(name)
+
